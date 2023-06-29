ae370ed1ab54fc378998e5cad64d0c4328045d58
JP-CC-Projects <joeparkcc@gmail.com>
2023-06-28 19:11:02 -0500

    added unit tests

--- src/main/java/com/coderscampus/arraylist/Main.java
+++ src/main/java/com/coderscampus/arraylist/Main.java
@@ -5,46 +5,31 @@ public class Main{
 
         CustomList<Integer> customList = new CustomArrayList();
 
-        // Testing zone: //
-        System.out.println("Boolean value = " + customList.add(1));
-        System.out.println("Array size/number of elements = " + customList.getSize());
-        Integer pickIndex = 0;
-        System.out.println("Element at index " + pickIndex + " = " + customList.get(pickIndex));
+//        // Testing zone: //
+//        System.out.println("Boolean value = " + customList.add(1));
+//        System.out.println("Array size/number of elements = " + customList.getSize());
+//        Integer pickIndex = 0;
+//        System.out.println("Element at index " + pickIndex + " = " + customList.get(pickIndex));
+//        customList.add(2);
+//        customList.add(3);
+//        customList.add(4);
+//        customList.add(5);
+//        customList.add(6);
+//        customList.add(7);
+//        customList.add(8);
+//        customList.add(9);
+//        customList.add(10);
+//        customList.add(11);
+//        customList.add(12);
+//        customList.add(13);
+//        customList.add(14);
+//        customList.add(15);
+//        customList.add(16);
+//        customList.add(17);
+//        customList.add(18);
+//        customList.add(19);
+//        customList.add(20);
 
-        customList.add(2);
-        customList.add(3);
-        customList.add(4);
-        customList.add(5);
-        customList.add(6);
-        customList.add(7);
-        customList.add(8);
-        customList.add(9);
-        customList.add(10);
-        customList.add(11);
-        customList.add(12);
-        customList.add(13);
-        customList.add(14);
-        customList.add(15);
-        customList.add(16);
-        customList.add(17);
-        customList.add(18);
-        customList.add(19);
-        customList.add(20);
-        customList.add(20, 100);
-        customList.remove(20);
-        customList.add(21);
-        customList.add(22);
-        customList.add(23);
-        customList.add(24);
-        customList.add(25);
-        customList.add(26);
-
-        System.out.println("Array size/number of elements = " + customList.getSize());
-        Integer pickIndex2 = 25;
-        System.out.println("Element at index " + pickIndex2 + " = " + customList.get(pickIndex2));
-        System.out.println("Removed element at index: " + pickIndex2 + " and returned " + customList.remove(pickIndex2));
-        System.out.println("New element at index " + pickIndex2 + " = " + customList.get(pickIndex2));
-        System.out.println(customList.get(pickIndex2));
 
     }
 }
---
+++ src/test/java/CustomArrayListTest.java
@@ -0,0 +1,89 @@
+import com.coderscampus.arraylist.CustomArrayList;
+import com.coderscampus.arraylist.CustomList;
+import org.junit.jupiter.api.Test;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class CustomArrayListTest {
+    @Test
+    void get_should_return_null_if_nothing_added(){
+        CustomList<Integer> customList = new CustomArrayList<Integer>();
+        assertNull(customList.get(0));
+        assertNull(customList.get(1));
+        assertNull(customList.get(2));
+    }
+
+    @Test
+    void getSize_should_return_0_if_nothing_added(){
+        CustomList<Integer> customList = new CustomArrayList<Integer>();
+        assertEquals(0, customList.getSize());
+    }
+
+    @Test
+    void should_return_true_when_successfully_adding_item(){
+        CustomList<Integer> customList = new CustomArrayList<Integer>();
+        assertTrue(customList.add(20));
+    }
+    @Test
+    void should_return_true_when_successfully_adding_index_item_to_exsting_array(){
+        CustomList<Integer> customList = new CustomArrayList<Integer>();
+        customList.add(0);
+        customList.add(1);
+        customList.add(2);
+        customList.add(3);
+        assertTrue(customList.add(2, 20));
+    }
+
+    @Test
+    void should_add_one_item_to_custom_arraylist(){
+        CustomList<Integer> customList = new CustomArrayList<Integer>();
+        customList.add(1);
+        assertEquals(1, customList.get(0));
+    }
+
+    void should_add_one_item_to_custom_arraylist_at_index_0(){
+        CustomList<Integer> customList = new CustomArrayList<Integer>();
+        customList.add(0, 1);
+        assertEquals(1, customList.get(0));
+        assertNull(customList.get(1));
+    }
+    @Test
+    void should_have_null_array_after_adding_and_removing_one_int(){
+        CustomList<Integer> customList = new CustomArrayList<Integer>();
+        customList.add(1);
+        customList.remove(0);
+        assertNull(customList.get(0));
+    }
+
+    @Test
+    void should_return_false_if_adding_to_nonexisting_index(){
+        CustomList<Integer> customList = new CustomArrayList<Integer>();
+        assertFalse(customList.add(2, 2));
+    }
+
+    @Test
+    void should_have_zero_sized_array_after_adding_and_removing_30_ints(){
+        CustomList<Integer> customList = new CustomArrayList<Integer>();
+        for(int i = 0; i < 30; i++){
+            customList.add(i, i);
+        }
+        for(int i = 0; i < 30; i++){
+            customList.remove(0);
+        }
+        assertEquals(0, customList.getSize());
+    }
+    @Test
+    void should_add_one_million_ints (){
+        CustomList<Integer> customListAddInts = new CustomArrayList<Integer>();
+        for(int i = 0; i < 1000000; i++){
+            customListAddInts.add(i);
+        }
+        assertEquals(1000000, customListAddInts.getSize());
+    }
+}
